a043f2153949b115ecd3b877422f48f087c249bc
Fatma Mohamed <157400371+FatmaMohamed4@users.noreply.github.com>
2024-06-21 04:23:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ import productRoute from './routes/productRoute.js';
 import categoryRoute from './routes/categoryRoute.js';
 import userRoute from './routes/userRoute.js';
 import globalError from './middlewares/errMiddleware.js';
-
+import cors from 'cors'
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/product',productRoute)
 app.use('/category', categoryRoute)
 app.use('/user',userRoute)
 
+app.use('/hello',(req,res)=>{
+  res.send("Hello")
+})
 app.use(globalError)
 
 app.use('*', (req, res) => {
