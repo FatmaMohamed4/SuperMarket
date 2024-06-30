3a6bb7fe2db3deeb2b8dc2c81c31edbbc172109d
Fatma Mohamed <157400371+FatmaMohamed4@users.noreply.github.com>
2024-06-30 03:05:39 +0300

    reset

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,16 @@
 import express from "express"
-import  { LogIn, register } from "../controllers/userController.js";
+import  { LogIn, forgotPassword, register, resetPassword, userInfo, verifyOTP } from "../controllers/userController.js";
+import protect from './../utils/protect.js';
 
 const userRoute = express.Router();
 
 
 userRoute.post('/register',register)
 userRoute.post('/login',LogIn)
+userRoute.get('/info',userInfo)
+
+userRoute.post('/sendCode', forgotPassword);
+userRoute.patch('/reset' ,protect,resetPassword)
+userRoute.post('/verify',verifyOTP)
 
 export default userRoute
---
+++ utils/Email.js
@@ -0,0 +1,30 @@
+
+import nodemailer from 'nodemailer'
+
+
+export const sendToEmail=(email, otp) =>{
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+            user: 'mail1project1@gmail.com', 
+            pass: 'dedbgjcvpeimmrwl'
+        }
+    });
+
+    // Email content
+    const mailOptions = {
+        from: 'mail1project1@gmail.com', 
+        to: email, 
+        subject: 'Your Reset Code  (OTP)', 
+        text: `Your OTP is: ${otp}` 
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error occurred while sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+    console.log(mailOptions)
+}
