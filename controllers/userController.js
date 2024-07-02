813923475ee4f1cda030488a935b7f42a0bedad8
Fatma Mohamed <157400371+FatmaMohamed4@users.noreply.github.com>
2024-07-02 15:38:26 +0300

    userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ const SECRET_Key = process.env.SECRET_Key;
       const email = req.body.email
         const user=await User.findOne({email})
         if(!user){
-           consr newUser = await User.create(req.body)
+           const newUser = await User.create(req.body)
             res.status(201).json({
                 status : true , 
                 message :"Register correctly" , newUser : newUser
