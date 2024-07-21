afe3d8fa74ec06924dc2565b1b91fa364bff4c32
Fatma Mohamed <157400371+FatmaMohamed4@users.noreply.github.com>
2024-07-21 04:04:44 +0300

    Update photoRoute.js

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import express from "express"
 
-import protect from "../utils/protect.js";
-import { UPLOADCategory, uploadPhoto } from "../controllers/photoController.js";
-
-
-const photoRoute = express.Router();
-
-photoRoute.post('/add/:id',protect, uploadPhoto)
-
-photoRoute.post('/category/:id', UPLOADCategory)
-
-=======
 import express from "express"
 
 import protect from "../utils/protect.js";
@@ -23,6 +10,4 @@ const photoRoute = express.Router();
 photoRoute.post('/add/:id',protect, uploadPhoto)
 
 
-
->>>>>>> 0ef84618eafe6293b6acaef03b2b7f60db094ded
 export default photoRoute
